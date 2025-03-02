85d73481107a5cb8cd52155b85e92bf2ea7a3813
derwout <wout.deruyck@gmail.com>
2025-03-02 21:44:13 +0100

    added bullets shot.py, asteroid destruction, updated circleshape.py, constants.py, main.py, player.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,12 +13,16 @@ def main():
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updatables, drawables)
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatables, drawables)
     AsteroidField.containers = updatables
+    asteroid_field = AsteroidField()
 
+    Player.containers = (updatables, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = AsteroidField()
+
+    Shot.containers = (shots, updatables, drawables)
 
     dt = 0
 
@@ -27,6 +32,15 @@ def main():
                 return
     
         updatables.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player) == True:
+                print("Game over!")
+                exit()
+            for shot in shots:
+                if shot.collision_check(asteroid) == True:
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +29,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_q]:
             self.rotate(-dt)
@@ -36,3 +39,12 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
+        bullet = Shot(self.position.x, self.position.y)
+        bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
